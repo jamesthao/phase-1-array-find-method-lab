7458f35aebd3fd87567c4f92bb9f8d6a5705f8f2
James <jamesthao32@gmail.com>
2022-03-07 00:04:16 -0700

    commit tree e550cc6b817110c393ae032da3773f5f18154077

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function superbowlWin(array) {
+    if (array.result === "W"){
+        return array.year
+    } else{
+        return undefined
+    }
+}
+
+array.find(superbowlWin).year
+
+
+
+function superbowlWin(arr) {
+    arr.find(function search(element) {
+      if(element.result === "W") {
+        console.log(element.year) 
+        console.log(typeof element.year) 
+        return element.year
+      }
+    });
+  }; 
